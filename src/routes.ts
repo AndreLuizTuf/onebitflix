176f6e6b018d760e0165c6aeb4ecb801f02fe3d0
André Luiz <andre.luiz.a.f@hotmail.com>
2023-06-20 15:46:25 -0300

    Obtendo a Lista de “Continuar Assistindo”

--- src/routes.ts
+++ src/routes.ts
@@ -6,6 +6,7 @@ import { authController } from './controllers/authController';
 import { ensureAuth, ensureAuthViaQuery } from './middlewares/auth';
 import { favoritesController } from './controllers/favoritesController';
 import { likesController } from './controllers/likesController';
+import { usersController } from './controllers/usersController';
 
 const router = express.Router();
 
@@ -39,5 +40,8 @@ router.post('/favorites', ensureAuth, favoritesController.save);
 router.delete('/favorites', ensureAuth, favoritesController.delete);
 router.post('/likes', ensureAuth, likesController.save);
 router.delete('/likes', ensureAuth, likesController.delete);
+
+router.get('/users/current/watching', ensureAuth, usersController.watching)
+
 
 export { router };
