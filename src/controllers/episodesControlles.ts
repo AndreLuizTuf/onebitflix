3e9453cc1253affea23b692a85bc1392dd7e1c53
André Luiz <andre.luiz.a.f@hotmail.com>
2023-06-20 14:39:47 -0300

    Adicionando o Progresso de um Episódio

--- src/controllers/episodesControlles.ts
+++ src/controllers/episodesControlles.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { episodeService } from '../services/episodeService';
+import { AuthenticatedRequest } from '../middlewares/auth';
 
 export const episodesController = {
   // GET /episodes/stream?videoUrl
@@ -10,10 +11,47 @@ export const episodesController = {
       if (typeof videoUrl !== 'string')
         throw new Error('videoUrl param must be of type string');
 
-        const range = req.headers.range; // bytes= 0-1024
+      const range = req.headers.range; // bytes= 0-1024
 
-        episodeService.streamEpisodeToResponde(res, videoUrl, range)
-      
+      episodeService.streamEpisodeToResponde(res, videoUrl, range);
+    } catch (err) {
+      if (err instanceof Error) {
+        return res.status(400).json({ message: err.message });
+      }
+    }
+  },
+
+  // GET /episodes/:id/watchTime
+  getWatchTime: async (req: AuthenticatedRequest, res: Response) => {
+    const userId = req.user!.id;
+    const episodeId = req.params.id;
+
+    try {
+      const watchTime = await episodeService.getWatchTime(
+        userId,
+        Number(episodeId)
+      );
+      return res.json(watchTime);
+    } catch (err) {
+      if (err instanceof Error) {
+        return res.status(400).json({ message: err.message });
+      }
+    }
+  },
+
+  // POST /episodes/:id/watchTime
+  setWatchTime: async (req: AuthenticatedRequest, res: Response) => {
+    const userId = req.user!.id;
+    const episodeId = Number(req.params.id);
+    const { seconds } = req.body;
+
+    try {
+      const watchTime = await episodeService.setWatchTime({
+        episodeId,
+        userId,
+        seconds,
+      });
+      return res.json(watchTime);
     } catch (err) {
       if (err instanceof Error) {
         return res.status(400).json({ message: err.message });
