15110f205b4c6cd6fbcf535d479b84923b4a542c
André Luiz <andre.luiz.a.f@hotmail.com>
2023-06-20 17:26:03 -0300

    Atualizando Informações do Usuário

--- src/routes.ts
+++ src/routes.ts
@@ -43,6 +43,7 @@ router.delete('/likes', ensureAuth, likesController.delete);
 
 router.get('/users/current', ensureAuth, usersController.show)
 router.get('/users/current/watching', ensureAuth, usersController.watching)
+router.put('/users/current', ensureAuth, usersController.update)
 
 
 export { router };
